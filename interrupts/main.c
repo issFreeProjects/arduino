2bdbd6741e3bc1e43ce81bb43317bb2131cc341c
iss <iss.siamak@gmail.com>
2022-08-25 18:26:17 +0430

    change speed button --to--> speed increaser button

--- interrupts/main.c
+++ interrupts/main.c
@@ -3,7 +3,7 @@
 
 /**
  *      blinking LED on B5 (internal arduino LED).
- *      speed button is connected to D2 port.
+ *      speed increaser button is connected to D2 port.
  *
  *              ____ D2
  *             |
@@ -13,10 +13,14 @@
  *           | |_____|1kΩ resistor|____ +5V
  *           |_______________________________GND
  *
- *      default blinking B5 (pin D13) 
- *          frequency is 1Hz.
- *      pressing sp_btn (speed btn) reduces
- *           by 
+ *      default blinking B5 (built in - pin D13)
+ *          blinking interval is 1 second.
+ *      pressing speed increaser button reduces
+ *          this time by 651/15624 ≈ 0.042(s)
+ *          so, 24 times pressing this button
+ *          will reset the speed to 1 second.
+ *
+ *
  * */
 
 #define F_CPU 16000000L
@@ -26,19 +30,19 @@
 // interrupt frequency, for more details see timer.pdf
 #define C1_MIN 49911
 #define C1_INTERVAL 15624   // equals C1_MAX-C1_MIN
-// pressing speed button will increase C1_MIN by
+// pressing speed increaser button will increase C1_MIN by
 // spbtn_counter*SPEED_SCALE that means at each time,
 // will reduce timer1's interrupt time interval by
 // SPEED_SCALE/C1_INTERVAL (s) = 0.041666 (s)
-// Note. 24 times pressing speed button, 
+// Note. 24 times pressing speed increaser button, 
 //       will set spbtn_counter=0.
 #define SPEED_SCALE 651
-// between any speed button interrupts, you need to
+// between any speed increaser button interrupts, you need to
 // make a delay about 200(ms) until button has released.
 // delay = BTN_DELAY/C1_INTERVAL = 0.199(s).
 #define BTN_DELAY 3124
 
-uint8_t spbtn_counter;
+uint8_t sib_c;   // speed increaser button counter
 
 
 
@@ -47,11 +51,11 @@ ISR(TIMER1_COMPA_vect)
     // toggleing B5 (D13 port)
     PORTB ^= _BV(PORTB5);
 
-    if(spbtn_counter*SPEED_SCALE >= C1_INTERVAL)
-        spbtn_counter = 0;
+    if(sib_c*SPEED_SCALE >= C1_INTERVAL)
+        sib_c = 0;
 
     // increase counter1's minimum by SPEED_SCALE
-    TCNT1  = C1_MIN + spbtn_counter*SPEED_SCALE;
+    TCNT1  = C1_MIN + sib_c*SPEED_SCALE;
 }
 
 
@@ -60,7 +64,7 @@ ISR(INT0_vect)
     int current_tcnt1;
 
     current_tcnt1 = TCNT1;
-    spbtn_counter++;
+    sib_c++;
 
     // about 200(ms) delay
     while(TCNT1-current_tcnt1 < BTN_DELAY){};
@@ -97,7 +101,7 @@ int main(void)
 {
     cli();
 
-    spbtn_counter = 0;
+    sib_c = 0;
 
     // make D13 (B5) as output and D* as input
     DDRB |= _BV(PORTB5);
